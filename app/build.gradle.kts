39bce73ce803f67bea2aed461d6aa67f3779fa94
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-08-03 15:07:37 +1000

    release: bump version to 6/1.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.nktnet.webview_locker"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "1.5"
+        versionCode = 6
+        versionName = "1.6"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
