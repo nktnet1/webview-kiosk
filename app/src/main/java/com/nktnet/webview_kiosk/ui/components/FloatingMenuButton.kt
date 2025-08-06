9bc905c84940658d414cc3b3c39bf855231c3e96
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-08-07 08:55:08 +1000

    feat: glowing floating menu button

--- app/src/main/java/com/nktnet/webview_kiosk/ui/components/FloatingMenuButton.kt
+++ app/src/main/java/com/nktnet/webview_kiosk/ui/components/FloatingMenuButton.kt
@@ -13,7 +13,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.graphics.nativeCanvas
+import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
@@ -21,6 +23,11 @@ import androidx.navigation.NavController
 import com.nktnet.webview_kiosk.config.Screen
 import kotlin.math.roundToInt
 import com.nktnet.webview_kiosk.config.SystemSettings
+import android.graphics.Paint
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.input.pointer.pointerInput
+
 
 @Composable
 fun FloatingMenuButton(
@@ -35,6 +42,7 @@ fun FloatingMenuButton(
     var offsetY by remember { mutableFloatStateOf(systemSettings.menuOffsetY) }
     var menuExpanded by remember { mutableStateOf(false) }
 
+    val primaryColor = MaterialTheme.colorScheme.primary
     val tintColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.8f)
 
     Box(
@@ -42,8 +50,28 @@ fun FloatingMenuButton(
             .offset { IntOffset(offsetX.roundToInt(), offsetY.roundToInt()) }
             .padding(24.dp)
             .size(64.dp)
+            .drawBehind {
+                val radiusPx = size.minDimension / 2
+                drawContext.canvas.nativeCanvas.apply {
+                    drawCircle(
+                        radiusPx,
+                        radiusPx,
+                        radiusPx,
+                        Paint().apply {
+                            color = primaryColor.toArgb()
+                            isAntiAlias = true
+                            setShadowLayer(
+                                20.dp.toPx(),
+                                0.dp.toPx(),
+                                0.dp.toPx(),
+                                primaryColor.toArgb()
+                            )
+                        }
+                    )
+                }
+            }
             .clip(CircleShape)
-            .background(MaterialTheme.colorScheme.primary)
+            .background(primaryColor)
             .pointerInput(Unit) {
                 detectDragGestures { change, dragAmount ->
                     change.consume()
