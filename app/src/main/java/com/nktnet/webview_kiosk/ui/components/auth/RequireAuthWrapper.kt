b584d19754c7baa1baef55f51daf281059384f1a
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-08-07 12:21:40 +1000

    fix: allow for no auth

--- app/src/main/java/com/nktnet/webview_kiosk/ui/components/auth/RequireAuthWrapper.kt
+++ app/src/main/java/com/nktnet/webview_kiosk/ui/components/auth/RequireAuthWrapper.kt
@@ -1,12 +1,15 @@
 package com.nktnet.webview_kiosk.ui.components.auth
 
+/*
 import android.content.Intent
 import android.os.Build
 import android.provider.Settings
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+*/
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
@@ -20,17 +23,21 @@ fun RequireAuthWrapper(
     promptManager: BiometricPromptManager,
     content: @Composable () -> Unit
 ) {
+    val showPrompt = remember {
+        {
+            promptManager.showBiometricPrompt(
+                title = "Authentication Required",
+                description = "Please authenticate to access settings"
+            )
+        }
+    }
+
     Box(modifier = Modifier.padding(top = 32.dp, start = 16.dp, end = 16.dp, bottom = 16.dp)) {
         RequireAuthentication(
             promptManager = promptManager,
             onAuthenticated = { content() },
             onFailed = { errorResult ->
-                AuthenticationErrorDisplay(errorResult = errorResult) {
-                    promptManager.showBiometricPrompt(
-                        title = "Authentication Required",
-                        description = "Please authenticate to access settings"
-                    )
-                }
+                AuthenticationErrorDisplay(errorResult = errorResult, onRetry = showPrompt)
             }
         )
     }
@@ -42,11 +49,12 @@ private fun RequireAuthentication(
     onFailed: @Composable (BiometricPromptManager.BiometricResult?) -> Unit
 ) {
     val biometricResult by promptManager.promptResults.collectAsState(initial = null)
+
+    /*
     val enrollLauncher = rememberLauncherForActivityResult(
         contract = ActivityResultContracts.StartActivityForResult(),
         onResult = { /* no-op */ }
     )
-
     LaunchedEffect(biometricResult) {
         if (biometricResult is BiometricPromptManager.BiometricResult.AuthenticationNotSet && Build.VERSION.SDK_INT >= 30) {
             enrollLauncher.launch(
@@ -59,6 +67,7 @@ private fun RequireAuthentication(
             )
         }
     }
+    */
 
     LaunchedEffect(Unit) {
         if (!promptManager.isAuthenticated) {
@@ -71,7 +80,9 @@ private fun RequireAuthentication(
 
     when {
         promptManager.isAuthenticated -> onAuthenticated()
-        biometricResult is BiometricPromptManager.BiometricResult.AuthenticationSuccess -> onAuthenticated()
+        biometricResult is BiometricPromptManager.BiometricResult.AuthenticationSuccess
+        || biometricResult is BiometricPromptManager.BiometricResult.AuthenticationNotSet
+            -> onAuthenticated()
         biometricResult == null -> LoadingIndicator("Waiting for authentication...")
         else -> onFailed(biometricResult)
     }
