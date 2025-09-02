0f9b87240475ae078ff7db29e8fcc3398b2f89e9
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-09-02 15:39:31 +1000

    refactor(ui): appearance screen ui

---
+++ app/src/main/java/com/nktnet/webview_kiosk/ui/components/setting/fields/appearance/BlockedMessageSetting.kt
@@ -0,0 +1,23 @@
+package com.nktnet.webview_kiosk.ui.components.setting.fields.appearance
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.platform.LocalContext
+import com.nktnet.webview_kiosk.config.UserSettings
+import com.nktnet.webview_kiosk.ui.components.common.settings.fields.TextSettingFieldItem
+
+@Composable
+fun BlockedMessageSetting() {
+    val context = LocalContext.current
+    val userSettings = remember { UserSettings(context) }
+
+    TextSettingFieldItem(
+        label = "Blocked Message",
+        infoText = "Custom message shown on blocked pages. Can be left empty.",
+        placeholder = "e.g. This site is blocked by <Company Name>",
+        initialValue = userSettings.blockedMessage,
+        isMultiline = true,
+        validator = { true },
+        onSave = { userSettings.blockedMessage = it }
+    )
+}
