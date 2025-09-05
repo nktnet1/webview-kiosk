0430f3a11af8a402a46888de47d85824c81831a1
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-09-05 11:00:42 +1000

    docs: added metadata for fdroid

--- docs/src/app/(hastopbar)/page.tsx
+++ docs/src/app/(hastopbar)/page.tsx
@@ -9,8 +9,8 @@ export default function HomePage() {
       <div className="bg-fd-muted rounded-2xl p-6 md:p-16">
         <h1 className="mb-8 text-4xl font-bold tracking-tight">{APP_NAME}</h1>
         <p className="mb-6 text-lg text-muted-foreground max-w-lg">
-          Turn any Android device into a dedicated, locked-down web page in
-          fullscreen mode.
+          Turn your Android device into a locked-down web page in fullscreen
+          mode
         </p>
         <div className="flex flex-wrap gap-2 items-center justify-center">
           <Button asChild variant="info" className="w-28">
