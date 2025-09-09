145822b7127e780ba1f8c81e5da9de44d2eaa09c
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-09-10 08:47:20 +1000

    Revert "chore: rename com.nktnet.webview_kiosk -> uk.nktnet.webviewkiosk (#2)"
    This reverts commit 142f5a38f077dbdcd066d8d600c6dafc10c9bdb3.
    
    Since package name cannot change on Playstore

---
+++ app/src/main/java/com/nktnet/webview_kiosk/utils/webview/getPrefersColorSchemeOverrideScript.kt
@@ -0,0 +1,49 @@
+package com.nktnet.webview_kiosk.utils.webview
+
+import com.nktnet.webview_kiosk.config.option.ThemeOption
+
+fun getPrefersColorSchemeOverrideScript(theme: ThemeOption): String {
+    if (theme == ThemeOption.SYSTEM) {
+        return ""
+    }
+
+    val mediaValue = when (theme) {
+        ThemeOption.DARK -> "dark"
+        ThemeOption.LIGHT -> "light"
+        else -> error("Unhandled theme: $theme")
+    }
+
+    return """
+        (function() {
+            window.matchMedia = (function(origMatchMedia) {
+                return function(query) {
+                    if (query === '(prefers-color-scheme: dark)') {
+                        return {
+                            matches: ${mediaValue == "dark"},
+                            media: query,
+                            addListener: function() {},
+                            removeListener: function() {},
+                            onchange: null,
+                            addEventListener: function() {},
+                            removeEventListener: function() {},
+                            dispatchEvent: function() { return false; }
+                        };
+                    }
+                    if (query === '(prefers-color-scheme: light)') {
+                        return {
+                            matches: ${mediaValue == "light"},
+                            media: query,
+                            addListener: function() {},
+                            removeListener: function() {},
+                            onchange: null,
+                            addEventListener: function() {},
+                            removeEventListener: function() {},
+                            dispatchEvent: function() { return false; }
+                        };
+                    }
+                    return origMatchMedia.call(window, query);
+                };
+            })(window.matchMedia);
+        })();
+    """.trimIndent()
+}
