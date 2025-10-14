682c93f8f651a0587045984b4b2fd581138200c3
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-14 18:11:43 +1100

    chore: remove toast for device owner receive

--- app/src/main/java/uk/nktnet/webviewkiosk/WebviewKioskAdminReceiver.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/WebviewKioskAdminReceiver.kt
@@ -3,12 +3,10 @@ package uk.nktnet.webviewkiosk
 import android.app.admin.DeviceAdminReceiver
 import android.content.Context
 import android.content.Intent
-import android.widget.Toast
 import uk.nktnet.webviewkiosk.utils.setupLockTaskPackage
 
 class WebviewKioskAdminReceiver : DeviceAdminReceiver() {
     override fun onEnabled(context: Context, intent: Intent) {
-        Toast.makeText(context, "Device Admin: Enabled", Toast.LENGTH_SHORT).show()
         setupLockTaskPackage(context)
         super.onEnabled(context, intent)
     }
