9c3ece3fbd24308f4b0df649fca9292214430f4a
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-17 23:31:27 +1100

    style: indentation for immersiveUtils.kt

--- app/src/main/java/uk/nktnet/webviewkiosk/utils/immersiveUtils.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/utils/immersiveUtils.kt
@@ -24,8 +24,8 @@ fun enterImmersiveMode(activity: Activity) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         activity.window.insetsController?.let { controller ->
             controller.hide(
-                WindowInsets.Type.statusBars() or
-                        WindowInsets.Type.navigationBars()
+                WindowInsets.Type.statusBars()
+                or WindowInsets.Type.navigationBars()
             )
             controller.systemBarsBehavior =
                 WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
@@ -34,20 +34,20 @@ fun enterImmersiveMode(activity: Activity) {
         @Suppress("DEPRECATION")
         activity.window.decorView.systemUiVisibility = (
             View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-            )
+            or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+            or View.SYSTEM_UI_FLAG_FULLSCREEN
+            or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
     }
 }
 
 fun exitImmersiveMode(activity: Activity) {
     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
         activity.window.insetsController?.show(
-            WindowInsets.Type.statusBars() or
-                    WindowInsets.Type.navigationBars()
+            WindowInsets.Type.statusBars()
+            or WindowInsets.Type.navigationBars()
         )
     } else {
         @Suppress("DEPRECATION")
