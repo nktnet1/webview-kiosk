f0db8fa62c0e2b6b62439fe7c721a5d23316444a
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-17 15:10:11 +1100

    refactor: abstract immersive utils out from component

--- app/src/main/java/uk/nktnet/webviewkiosk/utils/createCustomWebview.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/utils/createCustomWebview.kt
@@ -20,6 +20,8 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.core.net.toUri
+import enterImmersiveMode
+import exitImmersiveMode
 import uk.nktnet.webviewkiosk.config.Constants
 import uk.nktnet.webviewkiosk.config.UserSettings
 import uk.nktnet.webviewkiosk.config.option.ThemeOption
@@ -231,26 +233,7 @@ fun createCustomWebview(
                         )
                     )
 
-                    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                        activity.window.insetsController?.let { controller ->
-                            controller.hide(
-                                android.view.WindowInsets.Type.statusBars()
-                                    or android.view.WindowInsets.Type.navigationBars()
-                            )
-                            controller.systemBarsBehavior =
-                                android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
-                        }
-                    } else {
-                        @Suppress("DEPRECATION")
-                        activity.window.decorView.systemUiVisibility = (
-                            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                                or View.SYSTEM_UI_FLAG_FULLSCREEN
-                                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                            )
-                    }
+                    enterImmersiveMode(activity)
 
                     visibility = View.GONE
                     fullScreenContainer?.visibility = View.VISIBLE
@@ -265,14 +248,7 @@ fun createCustomWebview(
                     visibility = View.VISIBLE
                     customViewCallback?.onCustomViewHidden()
 
-                    activity?.let {
-                        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-                            it.window.insetsController?.show(android.view.WindowInsets.Type.statusBars() or android.view.WindowInsets.Type.navigationBars())
-                        } else {
-                            @Suppress("DEPRECATION")
-                            it.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
-                        }
-                    }
+                    activity?.let { exitImmersiveMode(it) }
                 }
             }
 
@@ -289,9 +265,9 @@ fun createCustomWebview(
             }
 
             setDownloadListener { url, _, _, _, _ ->
-                 config.showToast(
-                     "Downloading files is not yet supported in ${Constants.APP_NAME}."
-                 )
+                config.showToast(
+                    "Downloading files is not yet supported in ${Constants.APP_NAME}."
+                )
             }
         }
     }
--- app/src/main/java/uk/nktnet/webviewkiosk/utils/immersiveUtils.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/utils/immersiveUtils.kt
@@ -1,2 +1,37 @@
-package uk.nktnet.webviewkiosk.utils
+import android.app.Activity
+import android.view.View
 
+fun enterImmersiveMode(activity: Activity) {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        activity.window.insetsController?.let { controller ->
+            controller.hide(
+                android.view.WindowInsets.Type.statusBars() or
+                        android.view.WindowInsets.Type.navigationBars()
+            )
+            controller.systemBarsBehavior =
+                android.view.WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        }
+    } else {
+        @Suppress("DEPRECATION")
+        activity.window.decorView.systemUiVisibility = (
+            View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                or View.SYSTEM_UI_FLAG_FULLSCREEN
+                or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+        )
+    }
+}
+
+fun exitImmersiveMode(activity: Activity) {
+    if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
+        activity.window.insetsController?.show(
+            android.view.WindowInsets.Type.statusBars() or
+                    android.view.WindowInsets.Type.navigationBars()
+        )
+    } else {
+        @Suppress("DEPRECATION")
+        activity.window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_VISIBLE
+    }
+}
