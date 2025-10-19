7b79c5ba53bcd3638311fc252a13b530d13d17b0
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-20 09:43:11 +1100

    refactor: setup nav host abstracted out of main activity

--- app/src/main/java/uk/nktnet/webviewkiosk/MainActivity.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/MainActivity.kt
@@ -21,21 +21,17 @@ import androidx.compose.ui.Modifier
 import android.view.KeyEvent
 import androidx.core.view.WindowInsetsControllerCompat
 import androidx.navigation.NavHostController
-import androidx.navigation.compose.NavHost
-import androidx.navigation.compose.composable
-import androidx.navigation.compose.navigation
 import androidx.navigation.compose.rememberNavController
 import uk.nktnet.webviewkiosk.auth.BiometricPromptManager
 import uk.nktnet.webviewkiosk.config.*
 import uk.nktnet.webviewkiosk.config.option.DeviceRotationOption
 import uk.nktnet.webviewkiosk.config.option.ThemeOption
+import uk.nktnet.webviewkiosk.main.SetupNavHost
 import uk.nktnet.webviewkiosk.states.InactivityStateSingleton
 import uk.nktnet.webviewkiosk.states.LockStateSingleton
 import uk.nktnet.webviewkiosk.ui.components.webview.KeepScreenOnOption
 import uk.nktnet.webviewkiosk.ui.placeholders.UploadFileProgress
 import uk.nktnet.webviewkiosk.ui.theme.WebviewKioskTheme
-import uk.nktnet.webviewkiosk.ui.screens.*
-import uk.nktnet.webviewkiosk.utils.authComposable
 import uk.nktnet.webviewkiosk.utils.getLocalUrl
 import uk.nktnet.webviewkiosk.utils.getWebContentFilesDir
 import uk.nktnet.webviewkiosk.utils.handlePreviewKeyEvent
@@ -184,56 +180,6 @@ class MainActivity : AppCompatActivity() {
             ThemeOption.SYSTEM -> isSystemInDarkTheme()
             ThemeOption.DARK -> true
             ThemeOption.LIGHT -> false
-        }
-    }
-
-    @Composable
-    private fun SetupNavHost(
-        navController: NavHostController,
-        themeState: MutableState<ThemeOption>,
-        keepScreenOnState: MutableState<Boolean>,
-        deviceRotationState: MutableState<DeviceRotationOption>
-    ) {
-        NavHost(navController, startDestination = Screen.WebView.route) {
-            composable(Screen.WebView.route) {
-                WebviewScreen(navController)
-            }
-            composable(Screen.AdminRestrictionsChanged.route) {
-                AdminRestrictionsChangedScreen(navController)
-            }
-
-            navigation(startDestination = Screen.Settings.route, route = "settings_list") {
-                authComposable(Screen.Settings.route) {
-                    SettingsListScreen(navController, themeState = themeState)
-                }
-                authComposable(Screen.SettingsWebContent.route) {
-                    SettingsWebContentScreen(navController)
-                }
-                authComposable(Screen.SettingsWebContentFiles.route) {
-                    SettingsWebContentFilesScreen(navController)
-                }
-                authComposable(Screen.SettingsWebBrowsing.route) {
-                    SettingsWebBrowsingScreen(navController)
-                }
-                authComposable(Screen.SettingsWebEngine.route) {
-                    SettingsWebEngineScreen(navController)
-                }
-                authComposable(Screen.SettingsWebLifecycle.route) {
-                    SettingsWebLifecycleScreen(navController)
-                }
-                authComposable(Screen.SettingsAppearance.route) {
-                    SettingsAppearanceScreen(navController, themeState = themeState)
-                }
-                authComposable(Screen.SettingsDevice.route) {
-                    SettingsDeviceScreen(navController, keepScreenOnState, deviceRotationState)
-                }
-                authComposable(Screen.SettingsJsScript.route) {
-                    SettingsJsScriptsScreen(navController)
-                }
-                authComposable(Screen.SettingsAbout.route) {
-                    SettingsAboutScreen(navController)
-                }
-            }
         }
     }
 
---
+++ app/src/main/java/uk/nktnet/webviewkiosk/main/SetupNavHost.kt
@@ -0,0 +1,63 @@
+package uk.nktnet.webviewkiosk.main
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.navigation
+import uk.nktnet.webviewkiosk.config.Screen
+import uk.nktnet.webviewkiosk.config.option.DeviceRotationOption
+import uk.nktnet.webviewkiosk.config.option.ThemeOption
+import uk.nktnet.webviewkiosk.ui.screens.*
+import uk.nktnet.webviewkiosk.utils.authComposable
+
+@Composable
+fun SetupNavHost(
+    navController: NavHostController,
+    themeState: MutableState<ThemeOption>,
+    keepScreenOnState: MutableState<Boolean>,
+    deviceRotationState: MutableState<DeviceRotationOption>
+) {
+    NavHost(navController, startDestination = Screen.WebView.route) {
+        composable(Screen.WebView.route) {
+            WebviewScreen(navController)
+        }
+        composable(Screen.AdminRestrictionsChanged.route) {
+            AdminRestrictionsChangedScreen(navController)
+        }
+
+        navigation(startDestination = Screen.Settings.route, route = "settings_list") {
+            authComposable(Screen.Settings.route) {
+                SettingsListScreen(navController, themeState = themeState)
+            }
+            authComposable(Screen.SettingsWebContent.route) {
+                SettingsWebContentScreen(navController)
+            }
+            authComposable(Screen.SettingsWebContentFiles.route) {
+                SettingsWebContentFilesScreen(navController)
+            }
+            authComposable(Screen.SettingsWebBrowsing.route) {
+                SettingsWebBrowsingScreen(navController)
+            }
+            authComposable(Screen.SettingsWebEngine.route) {
+                SettingsWebEngineScreen(navController)
+            }
+            authComposable(Screen.SettingsWebLifecycle.route) {
+                SettingsWebLifecycleScreen(navController)
+            }
+            authComposable(Screen.SettingsAppearance.route) {
+                SettingsAppearanceScreen(navController, themeState = themeState)
+            }
+            authComposable(Screen.SettingsDevice.route) {
+                SettingsDeviceScreen(navController, keepScreenOnState, deviceRotationState)
+            }
+            authComposable(Screen.SettingsJsScript.route) {
+                SettingsJsScriptsScreen(navController)
+            }
+            authComposable(Screen.SettingsAbout.route) {
+                SettingsAboutScreen(navController)
+            }
+        }
+    }
+}
