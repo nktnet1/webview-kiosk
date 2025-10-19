0c63d87cc1899600de372feebc7d6e83a45849c2
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-20 00:51:00 +1100

    fix: setting actions icon occasionally trigger back navigation

--- app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/SettingLabel.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/SettingLabel.kt
@@ -14,40 +14,43 @@ import androidx.navigation.NavController
 import uk.nktnet.webviewkiosk.R
 import uk.nktnet.webviewkiosk.utils.navigateToWebViewScreen
 
+const val ICON_OFFSET = 8
+
 @Composable
 fun SettingLabel(
     navController: NavController,
     label: String,
     showBackIcon: Boolean = true,
     endIcon: @Composable (() -> Unit)? = {
-        Icon(
-            painter = painterResource(R.drawable.baseline_exit_to_app_24),
-            contentDescription = "Go to WebView",
-            modifier = Modifier.size(24.dp)
-        )
+        IconButton(
+            onClick = {
+                navigateToWebViewScreen(navController)
+            },
+        ) {
+            Icon(
+                painter = painterResource(R.drawable.baseline_exit_to_app_24),
+                contentDescription = "Go to WebView",
+            )
+        }
     },
-    onEndIconClick: (() -> Unit)? = {
-        navigateToWebViewScreen(navController)
-    }
 ) {
-    val sideOffset = 20.dp
     Row(
         verticalAlignment = Alignment.CenterVertically,
         modifier = Modifier
             .fillMaxWidth()
-            .padding(horizontal = 0.dp)
             .height(55.dp)
     ) {
         Row(verticalAlignment = Alignment.CenterVertically) {
             if (showBackIcon) {
                 IconButton(
                     onClick = { navController.popBackStack() },
-                    modifier = Modifier.size(64.dp).offset(x = -sideOffset),
+                    modifier = Modifier
+                        .padding(end = 1.dp)
+                        .offset(-ICON_OFFSET.dp)
                 ) {
                     Icon(
                         painter = painterResource(R.drawable.baseline_arrow_back_24),
                         contentDescription = "Back",
-                        modifier = Modifier.size(24.dp),
                     )
                 }
             }
@@ -56,16 +59,14 @@ fun SettingLabel(
                 text = label,
                 style = MaterialTheme.typography.headlineMedium,
                 color = MaterialTheme.colorScheme.onSurface,
-                modifier = Modifier.offset(x = -sideOffset)
             )
         }
 
         Spacer(modifier = Modifier.weight(1f))
 
         if (endIcon != null) {
-            IconButton(
-                onClick = { onEndIconClick?.invoke() },
-                modifier = Modifier.size(64.dp).offset(x = sideOffset),
+            Box(
+                modifier = Modifier.offset(ICON_OFFSET.dp)
             ) {
                 endIcon()
             }
