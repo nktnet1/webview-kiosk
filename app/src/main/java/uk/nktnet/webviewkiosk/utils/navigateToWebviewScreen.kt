dbb6143956f98ed8921a3c007caf0f296ac9e95f
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-10-22 20:33:38 +1100

    fix: launch single top when popping back to webview

--- app/src/main/java/uk/nktnet/webviewkiosk/utils/navigateToWebviewScreen.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/utils/navigateToWebviewScreen.kt
@@ -5,6 +5,9 @@ import uk.nktnet.webviewkiosk.config.Screen
 
 fun navigateToWebViewScreen(navController: NavController) {
     navController.navigate(Screen.WebView.route) {
-        popUpTo(Screen.Settings.route) { inclusive = true }
+        launchSingleTop = true
+        popUpTo(Screen.WebView.route) {
+            inclusive = true
+        }
     }
 }
