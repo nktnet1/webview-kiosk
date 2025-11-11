402e3e54abdc545c0f3dc9da90f2c3453ef2a44f
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-11 22:45:56 +1100

    v0.25.0.1-mqtt-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,7 +33,7 @@ android {
         minSdk = 21
         targetSdk = 36
         versionCode = 84
-        versionName = "0.25.0.0-mqtt-beta"
+        versionName = "0.25.0.1-mqtt-beta"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "${applicationId}-v${versionCode}-${versionName}")
 
