e18ee32faa32fbf90ed841bb4197f3bdd4963c78
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-13 22:11:05 +1100

    fix: managed configurations (app restrictions) - grab dynamically

--- app/src/main/java/uk/nktnet/webviewkiosk/utils/settingPreferenceUtils.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/utils/settingPreferenceUtils.kt
@@ -7,15 +7,15 @@ import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
 fun stringPref(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String,
     default: String
 ) = object : ReadWriteProperty<Any?, String> {
     override fun getValue(thisRef: Any?, property: KProperty<*>) =
-        if (restrictions?.containsKey(key) == true) {
+        if (getRestrictions()?.containsKey(key) == true) {
             try {
-                restrictions.getString(key)?.takeIf { it.isNotBlank() } ?: default
+                getRestrictions()?.getString(key)?.takeIf { it.isNotBlank() } ?: default
             } catch (_: Exception) {
                 prefs.edit { putString(key, default) }
                 default
@@ -25,20 +25,20 @@ fun stringPref(
         }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        if (restrictions?.containsKey(key) != true) {
+        if (getRestrictions()?.containsKey(key) != true) {
             prefs.edit { putString(key, value) }
         }
     }
 }
 
 fun stringPrefOptional(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String
 ) = object : ReadWriteProperty<Any?, String> {
     override fun getValue(thisRef: Any?, property: KProperty<*>) =
-        if (restrictions?.containsKey(key) == true) {
-            restrictions.getString(key) ?: ""
+        if (getRestrictions()?.containsKey(key) == true) {
+            getRestrictions()?.getString(key) ?: ""
         } else {
             try {
                 prefs.getString(key, null) ?: ""
@@ -49,19 +49,19 @@ fun stringPrefOptional(
         }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: String) {
-        if (restrictions?.containsKey(key) != true) prefs.edit { putString(key, value) }
+        if (getRestrictions()?.containsKey(key) != true) prefs.edit { putString(key, value) }
     }
 }
 
 fun booleanPref(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String,
     default: Boolean
 ) = object : ReadWriteProperty<Any?, Boolean> {
     override fun getValue(thisRef: Any?, property: KProperty<*>) =
-        if (restrictions?.containsKey(key) == true) {
-            restrictions.getBoolean(key)
+        if (getRestrictions()?.containsKey(key) == true) {
+            getRestrictions()?.getBoolean(key) ?: default
         } else {
             try {
                 prefs.getBoolean(key, default)
@@ -72,12 +72,12 @@ fun booleanPref(
         }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: Boolean) {
-        if (restrictions?.containsKey(key) != true) prefs.edit { putBoolean(key, value) }
+        if (getRestrictions()?.containsKey(key) != true) prefs.edit { putBoolean(key, value) }
     }
 }
 
 fun intPref(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String,
     default: Int,
@@ -85,8 +85,8 @@ fun intPref(
     max: Int = Int.MAX_VALUE
 ) = object : ReadWriteProperty<Any?, Int> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): Int {
-        val raw = if (restrictions?.containsKey(key) == true) {
-            restrictions.getInt(key)
+        val raw = if (getRestrictions()?.containsKey(key) == true) {
+            getRestrictions()?.getInt(key) ?: default
         } else {
             try {
                 prefs.getInt(key, default)
@@ -99,7 +99,7 @@ fun intPref(
     }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: Int) {
-        if (restrictions?.containsKey(key) != true) {
+        if (getRestrictions()?.containsKey(key) != true) {
             val coerced = value.coerceIn(min, max)
             prefs.edit { putInt(key, coerced) }
         }
@@ -107,14 +107,14 @@ fun intPref(
 }
 
 fun floatPref(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String,
     default: Float
 ) = object : ReadWriteProperty<Any?, Float> {
     override fun getValue(thisRef: Any?, property: KProperty<*>) =
-        if (restrictions?.containsKey(key) == true) {
-            restrictions.getFloat(key)
+        if (getRestrictions()?.containsKey(key) == true) {
+            getRestrictions()?.getFloat(key) ?: default
         } else {
             try {
                 prefs.getFloat(key, default)
@@ -125,22 +125,22 @@ fun floatPref(
         }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: Float) {
-        if (restrictions?.containsKey(key) != true) {
+        if (getRestrictions()?.containsKey(key) != true) {
             prefs.edit { putFloat(key, value) }
         }
     }
 }
 
 fun <T : Enum<T>> stringEnumPref(
-    restrictions: Bundle? = null,
+    getRestrictions: () -> Bundle? = { null },
     prefs: SharedPreferences,
     key: String,
     default: String,
     fromString: ((String?) -> T)
 ) = object : ReadWriteProperty<Any?, T> {
     override fun getValue(thisRef: Any?, property: KProperty<*>): T {
-        val value = if (restrictions?.containsKey(key) == true) {
-            restrictions.getString(key)
+        val value = if (getRestrictions()?.containsKey(key) == true) {
+            getRestrictions()?.getString(key)
         } else {
             try {
                 prefs.getString(key, default)
@@ -153,7 +153,7 @@ fun <T : Enum<T>> stringEnumPref(
     }
 
     override fun setValue(thisRef: Any?, property: KProperty<*>, value: T) {
-        if (restrictions?.containsKey(key) != true) {
+        if (getRestrictions()?.containsKey(key) != true) {
             prefs.edit { putString(key, value.name) }
         }
     }
