a29a6e562b3911b0c8252bdc995f09394195bf32
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-13 20:32:41 +1100

    docs: custom auth password

--- app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/device/CustomAuthPasswordSetting.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/device/CustomAuthPasswordSetting.kt
@@ -3,6 +3,7 @@ package uk.nktnet.webviewkiosk.ui.components.setting.fielditems.device
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import uk.nktnet.webviewkiosk.config.Constants
 import uk.nktnet.webviewkiosk.config.UserSettings
 import uk.nktnet.webviewkiosk.config.UserSettingsKeys
 import uk.nktnet.webviewkiosk.ui.components.setting.fields.TextSettingFieldItem
@@ -16,10 +17,19 @@ fun CustomAuthPasswordSetting() {
     TextSettingFieldItem(
         label = "Custom Auth Password",
         infoText = """
-            Specify a custom password to protect your settings or unlock the device.
-            Leave empty to use your device's password.
+            Specify a custom password to protect your settings or when unlocking from
+            the kiosk state.
+
+            For user-owned devices that utilises screen pinning, this will only work
+            if you are using an unlock method provided by the app.
+
+            Device-level unpin methods (e.g. gestures/holding overview + back button
+            simultaneously) will bypass this setting. To enhance security, please
+            see: ${Constants.WEBSITE_URL}/docs/security
+
+            Leave this setting blank to use your device's biometrics or credentials.
         """.trimIndent(),
-        placeholder = "(optional)",
+        placeholder = "(blank = device credentials)",
         initialValue = userSettings.customAuthPassword,
         restricted = restricted,
         isMultiline = false,
