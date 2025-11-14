22d10d9832b24ca1b080b76b75e41768e44c416a
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-14 12:15:34 +1100

    fix: reset authentication when new app restrictions are received

--- app/src/main/java/uk/nktnet/webviewkiosk/MainActivity.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/MainActivity.kt
@@ -60,6 +60,7 @@ class MainActivity : AppCompatActivity() {
                     navControllerState.value?.navigate(Screen.AdminRestrictionsChanged.route)
                 }
                 updateDeviceSettings(context)
+                AuthenticationManager.resetAuthentication()
             }
         }
     }
