12f54f4b3cf0f40e55ab062b2b367fc19d8789f0
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-22 00:41:30 +1100

    docs: notes about limitations

--- app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/webbrowsing/KioskControlPanelActionsSetting.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/webbrowsing/KioskControlPanelActionsSetting.kt
@@ -19,6 +19,13 @@ fun KioskControlPanelActionsSetting() {
             Manage visible actions in the kiosk control panel.
 
             Use the drag handle at the end to reorder the items.
+            
+            When in locked mode, the "UNLOCK" action will be appended to the
+            end if not configured.
+            
+            When in unlocked mode, if "Appearance -> Floating Toolbar Mode" is
+            set to Hidden, the "SETTINGS" action will be appended to the end
+            if not configured.
         """.trimIndent(),
         entries = KioskControlPanelActionOption.entries,
         getLabel = { it.label },
