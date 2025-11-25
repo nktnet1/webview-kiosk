dbd5f629b8cf42bab91d8361c7d1d5afbfb46f38
Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-11-25 16:31:47 +1100

    0.24.35

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "uk.nktnet.webviewkiosk"
         minSdk = 21
         targetSdk = 36
-        versionCode = 92
-        versionName = "0.24.34"
+        versionCode = 93
+        versionName = "0.24.35"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         setProperty("archivesBaseName", "${applicationId}-v${versionCode}-${versionName}")
 
