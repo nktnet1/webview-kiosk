fd5b83b21cbc90e81152e8015ab0f030bdf47e3d
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-12-20 03:13:15 +1100

    fix: switch to duckduckgo as the default search provider url

--- app/src/main/java/uk/nktnet/webviewkiosk/config/Constants.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/config/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val APP_NAME = "Webview Kiosk"
     const val WEBSITE_URL = "https://webviewkiosk.nktnet.uk"
     const val GITHUB_URL = "https://github.com/nktnet1/webview-kiosk"
-    const val DEFAULT_SEARCH_PROVIDER_URL = "https://google.com/search?q="
+    const val DEFAULT_SEARCH_PROVIDER_URL = "https://duckduckgo.com?q="
 
     const val MIN_INACTIVITY_TIMEOUT_SECONDS = 10
     const val INACTIVITY_COUNTDOWN_SECONDS = 5
