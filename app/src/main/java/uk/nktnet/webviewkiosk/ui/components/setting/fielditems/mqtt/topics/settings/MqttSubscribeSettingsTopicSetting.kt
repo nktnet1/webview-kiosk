1a7315a445d07fcae339aceb0d2a17caf6ae4bde
Khiet Tam Nguyen <86177399+nktnet1@users.noreply.github.com>
2025-12-22 18:17:57 +1100

    fix: improve topic name info text for subscribe topics (#176)
    Specifically:
    - wk/command
    - wk/settings
    - wk/request

--- app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/mqtt/topics/settings/MqttSubscribeSettingsTopicSetting.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/mqtt/topics/settings/MqttSubscribeSettingsTopicSetting.kt
@@ -7,6 +7,7 @@ import androidx.compose.ui.res.stringResource
 import uk.nktnet.webviewkiosk.R
 import uk.nktnet.webviewkiosk.config.UserSettings
 import uk.nktnet.webviewkiosk.config.UserSettingsKeys
+import uk.nktnet.webviewkiosk.config.mqtt.MqttVariableName
 import uk.nktnet.webviewkiosk.managers.MqttManager.mqttVariableReplacement
 import uk.nktnet.webviewkiosk.ui.components.setting.fields.TextSettingFieldItem
 import uk.nktnet.webviewkiosk.utils.isValidMqttSubscribeTopic
@@ -19,10 +20,17 @@ fun MqttSubscribeSettingsTopicSetting() {
 
     TextSettingFieldItem(
         label = stringResource(R.string.mqtt_subscribe_settings_topic_title),
-        infoText = """
-            The MQTT topic to subscribe for settings messages.
+        infoText = $$"""
+            The MQTT topic name to receive commands.
+
+            Supported variables:
+            - $${MqttVariableName.APP_INSTANCE_ID.name}
+            - $${MqttVariableName.USERNAME.name}
+
+            Example:
+            - wk/${$${MqttVariableName.APP_INSTANCE_ID.name}}/settings
         """.trimIndent(),
-        placeholder = "e.g. devices/+/settings",
+        placeholder = "e.g. wk/settings",
         initialValue = userSettings.mqttSubscribeSettingsTopic,
         validator = { it.isEmpty() || isValidMqttSubscribeTopic(it) },
         descriptionFormatter = {
