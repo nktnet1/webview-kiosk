ba40a57382bc3d9a9759ee9422333bf13aa4b3c1
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2025-12-26 23:21:27 +1100

    feat: unified push initial setting + screens

--- app/src/main/java/uk/nktnet/webviewkiosk/services/UnifiedPushService.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/services/UnifiedPushService.kt
@@ -4,10 +4,11 @@ import org.unifiedpush.android.connector.PushService
 import org.unifiedpush.android.connector.FailedReason
 import org.unifiedpush.android.connector.data.PushMessage
 import org.unifiedpush.android.connector.data.PushEndpoint
+import uk.nktnet.webviewkiosk.managers.ToastManager
 
 class UnifiedPushService : PushService() {
     override fun onMessage(message: PushMessage, instance: String) {
-
+        ToastManager.show(this, "$instance: ${message.content}")
     }
 
     override fun onNewEndpoint(endpoint: PushEndpoint, instance: String) {
