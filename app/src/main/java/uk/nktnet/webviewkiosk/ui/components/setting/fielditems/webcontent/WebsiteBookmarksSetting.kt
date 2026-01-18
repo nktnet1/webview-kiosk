d2e1c7825f7bd74e9bbaa187256ef34cc987784a
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-01-18 12:07:00 +1100

    feat: allow selecting bookmark from history

--- app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/webcontent/WebsiteBookmarksSetting.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/ui/components/setting/fielditems/webcontent/WebsiteBookmarksSetting.kt
@@ -9,7 +9,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -24,6 +27,7 @@ import uk.nktnet.webviewkiosk.config.SystemSettings
 import uk.nktnet.webviewkiosk.config.UserSettings
 import uk.nktnet.webviewkiosk.config.UserSettingsKeys
 import uk.nktnet.webviewkiosk.ui.components.setting.fields.TextSettingFieldItem
+import uk.nktnet.webviewkiosk.ui.components.webview.HistoryDialog
 import uk.nktnet.webviewkiosk.utils.validateUrl
 
 @Composable
@@ -60,17 +64,21 @@ fun WebsiteBookmarksSetting() {
             if (restricted) {
                 return@TextSettingFieldItem
             }
+
+            var isOpenHistoryDialog by remember { mutableStateOf(false) }
             val currentUrl = systemSettings.currentUrl
+            fun appendBookmark(newUrl: String) {
+                setValue(
+                    if (draftValue.isBlank()) {
+                        newUrl
+                    } else {
+                        "$draftValue\n$newUrl"
+                    }
+                )
+            }
+
             Button(
-                onClick = {
-                    setValue(
-                        if (draftValue.isBlank()) {
-                            currentUrl
-                        } else {
-                            "$draftValue\n$currentUrl"
-                        }
-                    )
-                },
+                onClick = { appendBookmark(currentUrl) },
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(top = 8.dp),
@@ -96,6 +104,32 @@ fun WebsiteBookmarksSetting() {
                     )
                 }
             }
+            Button(
+                onClick = { isOpenHistoryDialog = true },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp),
+                shape = RoundedCornerShape(4.dp),
+                colors = ButtonDefaults.buttonColors()
+            ) {
+                Text(
+                    text = "Select from History",
+                    style = MaterialTheme.typography.bodySmall,
+                    fontWeight = FontWeight.Bold,
+                    color = MaterialTheme.colorScheme.onPrimary,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.fillMaxWidth()
+                )
+            }
+
+            HistoryDialog(
+                isOpenHistoryDialog,
+                { isOpenHistoryDialog = false },
+                { item, _ ->
+                    appendBookmark(item.url)
+                },
+                false,
+            )
         }
     )
 }
