b5a5f994b4440ec2372349ee8a5d81a61ed1e23b
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-03-18 08:51:01 +1100

    deps: bump package versions + docs qr code v0.26.5

--- docs/src/components/qr/version.ts
+++ docs/src/components/qr/version.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = {
-  code: 118,
-  tag: "v0.26.4",
+  code: 119,
+  tag: "v0.26.5",
   adminSignatureChecksum: "L-EN4OxwoH84OoeJLKRWZyFOoTxO7qSjJU86Mxp6axU=",
 } as const;
