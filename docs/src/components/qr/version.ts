fc7fab76be4ae1e78d1ab07d06965372b4d15645
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-03-28 23:59:30 +1100

    docs: bump qr code install version to v0.26.6

--- docs/src/components/qr/version.ts
+++ docs/src/components/qr/version.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = {
-  code: 119,
-  tag: "v0.26.5",
+  code: 120,
+  tag: "v0.26.6",
   adminSignatureChecksum: "L-EN4OxwoH84OoeJLKRWZyFOoTxO7qSjJU86Mxp6axU=",
 } as const;
