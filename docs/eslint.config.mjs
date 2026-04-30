253125a5d8a447535804892133c32a87a20b6504
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-04-30 16:39:54 +1000

    deps: bump package versions

--- docs/eslint.config.mjs
+++ docs/eslint.config.mjs
@@ -13,6 +13,15 @@ const eslintConfig = defineConfig([
     "build/**",
     "next-env.d.ts",
   ]),
+  {
+    settings: {
+      react: {
+        // Use '19' instead of 'detect' as a workaround for Eslint v10:
+        // https://gist.github.com/OscarGauss/1f305edf5b7c103bb2ee32ba479f4261
+        version: "19"
+      }
+    }
+  }
 ]);
 
 export default eslintConfig;
