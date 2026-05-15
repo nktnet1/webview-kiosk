224e60fcb3086a92eb0404177f52e1b4c77c86f6
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-05-16 02:17:43 +1000

    docs: remove deprecated multiple() call in fumadocs-core

--- docs/src/lib/source.ts
+++ docs/src/lib/source.ts
@@ -1,11 +1,11 @@
-import { loader, multiple } from "fumadocs-core/source";
+import { loader } from "fumadocs-core/source";
 import { docs, legal as legalPages } from "fumadocs-mdx:collections/server";
 import { toFumadocsSource } from "fumadocs-mdx/runtime/server";
 
 export const source = loader(
-  multiple({
+  {
     docs: docs.toFumadocsSource(),
-  }),
+  },
   {
     baseUrl: "/docs",
   },
