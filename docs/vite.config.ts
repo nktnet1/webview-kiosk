694d243f7e470ff28af89d139d8d5b0143fc8f3d
Khiet Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-06-25 17:35:57 +1000

    fix: remove reliance on nitro

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,7 +2,6 @@ import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import react from "@vitejs/plugin-react";
 import mdx from "fumadocs-mdx/vite";
-import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 
 export default defineConfig({
@@ -37,11 +36,6 @@ export default defineConfig({
       ],
     }),
     react(),
-    nitro({
-      renderer: {
-        static: true,
-      }
-    }),
   ],
   resolve: {
     tsconfigPaths: true,
