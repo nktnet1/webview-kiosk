35a86a48e2112187bf55e3cf1f185d73d6e0ec9f
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-07-10 09:53:15 +1000

    chore: bump gradle version (9.6.1) and suppress deprecation for LayoutAlgorithm

--- app/src/main/java/uk/nktnet/webviewkiosk/config/option/LayoutAlgorithmOption.kt
+++ app/src/main/java/uk/nktnet/webviewkiosk/config/option/LayoutAlgorithmOption.kt
@@ -4,7 +4,9 @@ import android.webkit.WebSettings
 
 enum class LayoutAlgorithmOption(val algorithm: WebSettings.LayoutAlgorithm, val label: String) {
     NORMAL(WebSettings.LayoutAlgorithm.NORMAL, "Normal"),
+    @Suppress("DEPRECATION")
     SINGLE_COLUMN(WebSettings.LayoutAlgorithm.SINGLE_COLUMN, "Single Column"),
+    @Suppress("DEPRECATION")
     NARROW_COLUMNS(WebSettings.LayoutAlgorithm.NARROW_COLUMNS, "Narrow Columns"),
     TEXT_AUTOSIZING(WebSettings.LayoutAlgorithm.TEXT_AUTOSIZING, "Text Autosizing");
 
