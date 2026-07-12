731f4e20cea029bc99e29179e49a2b50081709a2
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-07-13 09:35:39 +1000

    docs: bump qr code installation version to 0.26.16 (130)

--- docs/src/components/qr/version.ts
+++ docs/src/components/qr/version.ts
@@ -1,5 +1,5 @@
 export const LATEST_VERSION = {
-  code: 128,
-  tag: "v0.26.14",
+  code: 130,
+  tag: "v0.26.16",
   adminSignatureChecksum: "L-EN4OxwoH84OoeJLKRWZyFOoTxO7qSjJU86Mxp6axU=",
 } as const;
