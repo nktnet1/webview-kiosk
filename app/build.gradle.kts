aa7ceca9ae1149436bb99025eb150f514d4d9410
Tam Nguyen <tamkhietnguyen1@gmail.com>
2026-09-23 10:01:08 +1000

    0.26.21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "uk.nktnet.webviewkiosk"
         minSdk = 21
         targetSdk = 37
-        versionCode = 134
-        versionName = "0.26.20"
+        versionCode = 135
+        versionName = "0.26.21"
         buildConfigField("int", "MIN_SDK_VERSION", "$minSdk")
     }
 
